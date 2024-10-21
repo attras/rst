47812976c22b5e7f8895ea2cda60bd41f873e5a3
Nanang1810 <saputraanang261@gmail.com>
2024-10-21 13:50:23 +0700

    front

--- app_setori/views/data_pokok.py
+++ app_setori/views/data_pokok.py
@@ -7,3 +7,7 @@ class Data_pokokViews(View):
     def get(self, request):
         
         return render(request, 'setori/data_pokok/index.html')
+
+class DetaildataViews(View):
+    def get(self, request):
+        return render(request, 'setori/data_pokok/data_rinci.html')
--- app_setori/views/potensi.py
+++ app_setori/views/potensi.py
@@ -7,6 +7,14 @@ class PotensiViews(View):
     def get(self, request):
         return render(request, 'setori/potensidaerah/index.html')
 
-class DetailpotensiViews(View):
+class DistrikpotensiViews(View):
+    def get(self, request):
+        return render(request, 'setori/potensidaerah/distrik_lengkap.html')
+
+class DetailpotensidistrikViews(View):
+    def get(self, request):
+        return render(request, 'setori/potensidaerah/detaildistrik.html')
+
+class DetailpotensikampungViews(View):
     def get(self, request):
         return render(request, 'setori/potensidaerah/detailkampung.html')
