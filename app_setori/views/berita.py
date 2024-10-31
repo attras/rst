a8883b92eff9b309e83cb2343b13dc784eb2f1da
Nanang1810 <saputraanang261@gmail.com>
2024-10-31 11:12:46 +0700

    update

--- app_setori/views/berita.py
+++ app_setori/views/berita.py
@@ -30,33 +30,36 @@ from django.core.paginator import Paginator
 
 class BeritaViews(View):
     def get(self, request):
-        dt_berita = News.objects.filter(deleted_at__isnull=True)  # Ambil semua berita yang tidak dihapus
+        dt_berita = News.objects.filter(deleted_at__isnull=True).order_by('-created_at')  # Ambil semua berita yang tidak dihapus
         dt_kategori = Category.objects.filter(deleted_at__isnull=True)  # Ambil semua kategori yang tidak dihapus
 
-        paginator = Paginator(dt_berita, 6)  # Batasi jumlah berita yang ditampilkan per halaman (6 berita)
-        page_number = request.GET.get('page')  # Ambil nomor halaman dari query parameter
-        dt_berita_paginated = paginator.get_page(page_number)  # Ambil berita untuk halaman yang diminta
+        paginator = Paginator(dt_berita, 6)  # Batasi 10 berita per halaman
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
 
         data = {
-            'dt_berita': dt_berita_paginated,  # Ganti dengan data yang sudah dipaginate
-            'dt_kategori': dt_kategori
+            'dt_berita': dt_berita,  # Ganti dengan data yang sudah dipaginate
+            'dt_kategori': dt_kategori, 
+            'page_obj': page_obj
         }
         return render(request, 'setori/berita/index.html', data)
 
 class DetailberitaViews(View):
     def get(self, request, slug):
         detail_berita = get_object_or_404(News,slug=slug)
-        dt_berita = News.objects.filter(deleted_at__isnull=True)  # Ambil semua berita yang tidak dihapus
+        dt_berita = News.objects.filter(deleted_at__isnull=True).order_by('-created_at')  # Ambil semua berita yang tidak dihapus
         dt_kategori = Category.objects.filter(deleted_at__isnull=True)  # Ambil semua kategori yang tidak dihapus
 
         paginator = Paginator(dt_berita, 6)  # Batasi jumlah berita yang ditampilkan per halaman (6 berita)
         page_number = request.GET.get('page')  # Ambil nomor halaman dari query parameter
-        dt_berita_paginated = paginator.get_page(page_number)  # Ambil berita untuk halaman yang diminta
+        page_obj = paginator.get_page(page_number)  # Ambil berita untuk halaman yang diminta
 
         data = {
-            'dt_berita': dt_berita_paginated,
+            'dt_berita': dt_berita,
             'detail_berita': detail_berita,
-            'dt_kategori': dt_kategori
+            'dt_kategori': dt_kategori,
+            'page_obj': page_obj
         }
         return render(request, 'setori/berita/detail_berita.html', data)
      
